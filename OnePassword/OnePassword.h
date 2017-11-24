39a0dc49140fecd2ea2853e40a6ed3d7ecbcec6a
Christopher Beloch <contact@cbeloch.de>
2017-11-24 14:31:24 +0100

    begin with a new Framework

---
+++ OnePassword/OnePassword.h
@@ -0,0 +1,19 @@
+//
+//  OnePassword.h
+//  OnePassword
+//
+//  Created by Christopher Beloch on 24.11.17.
+//  Copyright © 2017 Some Kind of Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnePassword.
+FOUNDATION_EXPORT double OnePasswordVersionNumber;
+
+//! Project version string for OnePassword.
+FOUNDATION_EXPORT const unsigned char OnePasswordVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnePassword/PublicHeader.h>
+
+
